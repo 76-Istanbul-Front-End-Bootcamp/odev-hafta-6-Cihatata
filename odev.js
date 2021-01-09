2306451ea46641ea83877f0100646994323697e3
cihatata <omercihatataa@gmail.com>
2021-01-09 22:17:44 +0300

    completed

--- odev.js
+++ odev.js
@@ -1,9 +1,44 @@
-window.mockApiUrl = "https://5ff193d0db1158001748b15e.mockapi.io/pets/";
+window.mockApiUrl = "https://5ff1abf3db1158001748b43c.mockapi.io/pets/";
+
+const modalDiv = document.createElement('div')
+document.querySelector('body').appendChild(modalDiv);
 
 window.removePet = (id) => {
-    console.log(id) // remove the pet with given id
+    // remove the pet with given id
+    fetch(`${window.mockApiUrl}${id}`, {
+            method: "DELETE",
+            headers: {
+                'Accept': "application/json",
+                'Content-Type': 'application/json'
+            }
+        }).then(() => {
+            window.location.reload()
+        })
+        .catch((err) => console.log(err));
 };
 
-window.openPetDetail = (id) => {
-    console.log(id); // show details of the pet with given id
+window.openPetDetail = async (id) => {
+    const res = await fetch(`${window.mockApiUrl}${id}`)
+    const data = await res.json();
+    const modal = showDetail(data);
+    modalDiv.innerHTML = modal;
+    $(`#${id}`).modal("show");
 };
+
+function showDetail(pet) {
+    return (`
+    <div class="modal fade" id="${pet.id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+             <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+           </div>
+        <div class="modal-body">
+         <img src="${pet.image}" alt="petimage" />
+          ${pet.name}</br>${pet.description}
+         </div>
+        </div>
+    </div>
+  </div>
+    `)
+}
